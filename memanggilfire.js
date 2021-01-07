ab8e6cd3ce0a5f3f20a49ffca33215b0b6d9549e
zaylani99 <jayijay786@gmail.com>
2021-01-07 19:13:33 +0800

    tugas terakhir pemweb

---
+++ memanggilfire.js
@@ -0,0 +1,12 @@
+  var firebaseConfig = {
+    apiKey: "AIzaSyCqrVLPwqMz3m0YN6HTnVD2KH4ZQsr6lNE",
+    authDomain: "programmenghitungkuadrat.firebaseapp.com",
+    projectId: "programmenghitungkuadrat",
+    storageBucket: "programmenghitungkuadrat.appspot.com",
+    messagingSenderId: "1009572444302",
+    appId: "1:1009572444302:web:65a48c724f68c0736631df",
+    measurementId: "G-6RCK4P3C3M"
+  };
+  firebase.initializeApp(firebaseConfig);
+ 
+ const database = firebase.database();
